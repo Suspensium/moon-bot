4ba510ca1891c9056ce9e25650b3e9aed06d332a
Suspensium <vitaliysmorodinov1@gmail.com>
2023-04-24 19:15:15 +0300

    sorting of achievements

--- commands/get/achievements.js
+++ commands/get/achievements.js
@@ -18,12 +18,13 @@ module.exports = {
         }
 
         const achievements = await getAchievements(user);
+        achievements.sort((a, b) => a.number - b.number);
 
         const achievementsCard = new EmbedBuilder()
             .setColor(0x0099FF)
             .setTitle(user.username);
 
-        for (let i = 0; i < (await achievements).length; i++) {
+        for (let i = 0; i < achievements.length; i++) {
             achievementsCard.addFields({ name: `${achievements[i].number}. ${achievements[i].name}`, value: `${achievements[i].description}` })
         }
 
