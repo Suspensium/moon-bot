0d197a0c45e6d5cbc80cf09a28f5e72bd79343be
Suspensium <vitaliysmorodinov1@gmail.com>
2023-05-01 17:29:22 +0300

    added query for some functions

--- commands/set/add_coins.js
+++ commands/set/add_coins.js
@@ -1,15 +1,14 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const { addBalance } = require('../../scripts/accrue.js');
-const { getBalance } = require('../../scripts/getInfo.js');
 const { userExists } = require('../../scripts/userExists.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('add_coins')
-        .setDescription("Adds coins to a user's balance")
+        .setDescription("Adds coins to user(s) balance")
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addUserOption(option =>
-            option.setName('user')
+        .addStringOption(option =>
+            option.setName('users')
                 .setDescription('Guild member name')
                 .setRequired(true))
         .addIntegerOption(option =>
@@ -17,14 +16,19 @@ module.exports = {
                 .setDescription('The amount of currency to accrue')
                 .setRequired(true)),
     async execute(interaction) {
-        const user = interaction.options.getUser('user');
+        const mentionedUsersIds = interaction.options.getString('users').match(/<@!?(\d+)>/g).map(mention => mention.replace(/[<@!>]/g, ''));
+        const mentionedMembers = await interaction.guild.members.fetch({ user: mentionedUsersIds, cache: true });
+        const mentionedUsers = mentionedMembers.map(member => member.user);
+        let users = [];
         const currency = interaction.options.getInteger('currency');
-
-        if (!(await userExists(user))) {
-            await interaction.reply(`Пользователь ${user.toString()} не найден в базе данных.`);
-            return;
+        for (const user of mentionedUsers) {
+            if (!(await userExists(user))) {
+                await interaction.channel.send(`Пользователь ${user.toString()} не найден в базе данных.`);
+                continue;
+            }
+            users.push(user.toString());
+            await addBalance(user, currency);
         }
-        await addBalance(user, currency);
-        await interaction.reply(`Баланс ${user.toString()} был изменен на ${currency}. Новый баланс с учётом коэффициента: ${await getBalance(user)}.`);
+        await interaction.reply(`Баланс ${users} был изменен на ${currency}.`);
     },
 };
--- commands/set/add_level.js
+++ commands/set/add_level.js
@@ -1,29 +1,34 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const { addLevel } = require('../../scripts/accrue.js');
-const { getLevel } = require('../../scripts/getInfo.js');
 const { userExists } = require('../../scripts/userExists.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('add_level')
-        .setDescription("Adds levels to a user")
+        .setDescription("Changes the level of user(s)")
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addUserOption(option =>
-            option.setName('user')
+        .addStringOption(option =>
+            option.setName('users')
                 .setDescription('Guild member name')
                 .setRequired(true))
         .addIntegerOption(option =>
             option.setName('level')
-                .setDescription('The amount of levels to accrue')),
+                .setDescription('Level to add')
+                .setRequired(true)),
     async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        const level = interaction.options.getInteger('level') ?? 1;
-
-        if (!(await userExists(user))) {
-            await interaction.reply(`Пользователь ${user.toString()} не найден в базе данных.`);
-            return;
+        const mentionedUsersIds = interaction.options.getString('users').match(/<@!?(\d+)>/g).map(mention => mention.replace(/[<@!>]/g, ''));
+        const mentionedMembers = await interaction.guild.members.fetch({ user: mentionedUsersIds, cache: true });
+        const mentionedUsers = mentionedMembers.map(member => member.user);
+        let users = [];
+        const level = interaction.options.getInteger('level');
+        for (const user of mentionedUsers) {
+            if (!(await userExists(user))) {
+                await interaction.channel.send(`Пользователь ${user.toString()} не найден в базе данных.`);
+                continue;
+            }
+            users.push(user.toString());
+            await addLevel(user, level);
         }
-        await addLevel(user, level);
-        await interaction.reply(`Уровень ${user.toString()} был повышен на ${level} до ${await getLevel(user)}.`);
+        await interaction.reply(`Уровень ${users} был изменен на ${level}.`);
     },
 };
