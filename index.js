5fa62e42e1886e890992fda80150ffe2395cae12
Suspensium <vitaliysmorodinov1@gmail.com>
2023-05-06 16:46:10 +0300

    interactionTimeout increased

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages], interactionTimeout: 10000 });
 client.commands = new Collection();
 
 // initialize commands
