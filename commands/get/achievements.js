f3145919ef904be85df77044d07d349ae3f6ec9e
Suspensium <vitaliysmorodinov1@gmail.com>
2023-06-07 11:31:12 +0300

    fixed achievements

--- commands/get/achievements.js
+++ commands/get/achievements.js
@@ -11,23 +11,58 @@ module.exports = {
                 .setDescription('Guild member name')),
     async execute(interaction) {
         const user = interaction.options.getUser('user') ?? interaction.user;
-
         if (!(await userExists(user))) {
             await interaction.reply(`Пользователь ${user.toString()} не найден в базе данных.`);
             return;
         }
+
+        const channel = interaction.channel;
+
         const achievements = await getAchievements(user);
         achievements.sort((a, b) => a.number - b.number);
 
-        const achievementsCard = new EmbedBuilder()
+        const embeds = [];
+        let currentEmbedFields = 0;
+        let currentEmbed = new EmbedBuilder()
             .setColor(0x0099FF)
             .setTitle(user.username)
             .setThumbnail(user.avatarURL());
 
         for (let i = 0; i < achievements.length; i++) {
-            achievementsCard.addFields({ name: `${achievements[i].number}. ${achievements[i].name}`, value: `${achievements[i].description}` })
+            if (currentEmbedFields >= 25) {
+                currentEmbedFields = 0;
+                embeds.push(currentEmbed);
+                currentEmbed = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle(user.username)
+                    .setThumbnail(user.avatarURL());
+            }
+            currentEmbedFields++;
+            currentEmbed.addFields({ name: `${achievements[i].number}. ${achievements[i].name}`, value: `${achievements[i].description}` });
         }
 
-        await interaction.reply({ embeds: [achievementsCard] });
+        embeds.push(currentEmbed);
+
+        try {
+            for (let i = 0; i < embeds.length; i++) {
+                const embed = embeds[i];
+                if (i === 0) {
+                    await interaction.reply({ embeds: [embed] });
+                } else {
+                    await interaction.followUp({ embeds: [embed] });
+                }
+            }
+        } catch (error) {
+            if (error.code === 10008 || error.code === 10062) {
+                // Interaction timed out
+                for (let i = 0; i < embeds.length; i++) {
+                    const embed = embeds[i];
+                    channel.send({ embeds: [embed] });
+                }
+            } else {
+                console.error(error);
+                channel.send('Произошла ошибка при выполнении команды.');
+            }
+        }
     },
 };
