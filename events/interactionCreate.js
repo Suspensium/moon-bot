88b1b4fe3a47a00fa8ad363704dc349df4ad1563
Suspense <vitaliysmorodinov1@gmail.com>
2023-07-06 00:29:34 +0300

    Fixes

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -35,52 +35,47 @@ module.exports = {
 
             // role
             if (interaction.customId.startsWith('role_')) {
-                const roleId = interaction.customId.replace('role_', '');
-                const role = interaction.guild.roles.cache.get(roleId);
-
-                if (!role) {
-                    await interaction.reply({ content: 'Роль не найдена!', ephemeral: true });
-                    return;
-                }
-
                 try {
+                    const roleId = interaction.customId.replace('role_', '');
+                    const role = interaction.guild.roles.cache.get(roleId);
+
+                    if (!role) {
+                        await interaction.reply({ content: 'Роль не найдена!', ephemeral: true });
+                        return;
+                    }
+
                     await interaction.member.roles.add(role);
                     await interaction.reply({ content: `Роль "${role.name}" успешно добавлена.`, ephemeral: true });
                 } catch (error) {
                     console.error('Failed to add role:\n', error);
-                    await interaction.reply({ content: 'Произошла ошибка в выполнении команды.', ephemeral: true });
                 }
                 return;
             }
 
             // daily
             if (interaction.customId === 'daily') {
-                const dailyAccrue = 20;
-                if (!(await userExists(interaction.user))) {
-                    await addUser(interaction.user, 1, 0);
-                }
-
-                const user = await getUser(interaction.user.id);
-                const lastClaimedDate = user.lastDaily ? moment(user.lastDaily).tz('Europe/Moscow').format('YYYY-MM-DD') : null;
-                const currentDate = moment().tz('Europe/Moscow').format('YYYY-MM-DD');
-
-                if (lastClaimedDate === currentDate) {
-                    try {
-                        await interaction.reply({ content: 'Ты уже отмечался сегодня.', ephemeral: true });
-                    } catch (error) {
-                    console.error('Failed to add daily currency:\n', error);
-                }
-                    return;
-                }
-
-                user.lastDaily = moment().valueOf();
-                await user.save();
-
-                let coef = '1';
-                if (user.level >= 10 && user.level < 25) coef = '1.25'; else if (user.level > 25) coef = '1.5';
-
-                await addBalance(user, dailyAccrue);
                 try {
+                    const dailyAccrue = 20;
+                    if (!(await userExists(interaction.user))) {
+                        await addUser(interaction.user, 1, 0);
+                    }
+
+                    const user = await getUser(interaction.user.id);
+                    const lastClaimedDate = user.lastDaily ? moment(user.lastDaily).tz('Europe/Moscow').format('YYYY-MM-DD') : null;
+                    const currentDate = moment().tz('Europe/Moscow').format('YYYY-MM-DD');
+
+                    if (lastClaimedDate === currentDate) {
+                        await interaction.reply({ content: 'Ты уже отмечался сегодня.', ephemeral: true });
+                        return;
+                    }
+
+                    user.lastDaily = moment().valueOf();
+                    await user.save();
+
+                    let coef = '1';
+                    if (user.level >= 10 && user.level < 25) coef = '1.25'; else if (user.level > 25) coef = '1.5';
+
+                    await addBalance(user, dailyAccrue);
                     await interaction.reply({ content: `Ты успешно отметился сегодня, получая ${dailyAccrue} x ${coef} мункойнов.`, ephemeral: true });
                 } catch (error) {
                     console.error('Failed to add daily currency:\n', error);
@@ -90,31 +85,31 @@ module.exports = {
 
             // accrue
             if (interaction.customId.startsWith('accrue_')) {
-                const currency = interaction.customId.replace('accrue_', '');
-
-                if (!(await userExists(interaction.user))) {
-                    await addUser(interaction.user, 1, 0);
-                }
-
-                const clickedButton = await getButton(interaction.message.id);
-
-                if (!clickedButton) {
-                    await addButton(interaction.message.id);
-                }
-
-                if (await getButtonUser(interaction.message.id, interaction.user.id)) {
-                    await interaction.reply({ content: 'Ты уже отметился.', ephemeral: true });
-                    return;
-                }
-
-                await addUserToButton(interaction.message.id, interaction.user.id);
-
-                const level = await getLevel(interaction.user);
-                let coef = '1';
-                if (level >= 10 && level < 25) coef = '1.25'; else if (level > 25) coef = '1.5';
-
-                await addBalance(interaction.user, currency);
                 try {
+                    const currency = interaction.customId.replace('accrue_', '');
+
+                    if (!(await userExists(interaction.user))) {
+                        await addUser(interaction.user, 1, 0);
+                    }
+
+                    const clickedButton = await getButton(interaction.message.id);
+
+                    if (!clickedButton) {
+                        await addButton(interaction.message.id);
+                    }
+
+                    if (await getButtonUser(interaction.message.id, interaction.user.id)) {
+                        await interaction.reply({ content: 'Ты уже отметился.', ephemeral: true });
+                        return;
+                    }
+
+                    await addUserToButton(interaction.message.id, interaction.user.id);
+
+                    const level = await getLevel(interaction.user);
+                    let coef = '1';
+                    if (level >= 10 && level < 25) coef = '1.25'; else if (level > 25) coef = '1.5';
+
+                    await addBalance(interaction.user, currency);
                     await interaction.reply(`${interaction.user.toString()} подтвердил присутствие на РТ, получая ${currency} x ${coef} мункойнов.`);
                 } catch (error) {
                     console.error('Failed to accrue:\n', error);
@@ -124,14 +119,13 @@ module.exports = {
 
             // register
             if (interaction.customId === 'register') {
-                if (await userExists(interaction.user)) {
-                    await interaction.reply({ content: `Ты уже зарегистрирован.`, ephemeral: true });
-                    return;
-                }
-
-                await addUser(interaction.user, 1, 0);
-
                 try {
+                    if (await userExists(interaction.user)) {
+                        await interaction.reply({ content: `Ты уже зарегистрирован.`, ephemeral: true });
+                        return;
+                    }
+
+                    await addUser(interaction.user, 1, 0);
                     await interaction.reply({ content: `Ты успешно зарегистрировался.`, ephemeral: true });
                 } catch (error) {
                     console.error('Failed to register:\n', error);
